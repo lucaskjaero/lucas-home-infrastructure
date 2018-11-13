b9b80540e19a7396f2e077a23520534800c3b432
Lucas Kjaero <lucas@lucaskjaero.com>
2018-11-12 19:14:58 -0800

    Add some ssh stuff (incomplete)

--- cookbooks/atlantis_ftp_connection/recipes/default.rb
+++ cookbooks/atlantis_ftp_connection/recipes/default.rb
@@ -5,3 +5,17 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 package 'sshfs'
+
+directory '/Atlantis' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+mount 'atlantis' do
+  device 'fuse'
+  enabled true
+  fstype 'sshfs#sanjid@atlantis.whatbox.ca:/home/sanjid/files'
+  mount_point '/Atlantis'
+end
