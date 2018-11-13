7262415186ba683eabfe2c84b6ac5ba21c323489
Lucas Kjaero <lucas@lucaskjaero.com>
2018-11-12 22:43:02 -0800

    Forgot about known hosts

--- cookbooks/atlantis_ftp_connection/recipes/default.rb
+++ cookbooks/atlantis_ftp_connection/recipes/default.rb
@@ -20,27 +20,40 @@ directory '/Atlantis' do
   not_if { Dir.exist? '/Atlantis' }
 end
 
+# Handle first time connection -- gotta accept the host name
+directory '/root/.ssh' do
+  owner 'root'
+  group 'root'
+  mode '0777'
+  action :create
+  not_if { Dir.exist? '/root/.ssh' }
+end
+cookbook_file '/root/.ssh/known_hosts' do
+  source 'known_hosts'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+# Set up public key auth to connect to atlantis
 directory '/home/atlantis/' do
   owner 'atlantis'
   group 'atlantis'
   mode '0755'
   action :create
 end
-
 directory '/home/atlantis/.ssh' do
   owner 'atlantis'
   group 'atlantis'
   mode '0755'
   action :create
 end
-
 cookbook_file '/home/atlantis/.ssh/id_rsa' do
   source 'privatekey'
   owner 'atlantis'
   group 'atlantis'
   mode '0755'
 end
-
 cookbook_file '/home/atlantis/.ssh/id_rsa.pub' do
   source 'publickey'
   owner 'atlantis'
@@ -48,11 +61,11 @@ cookbook_file '/home/atlantis/.ssh/id_rsa.pub' do
   mode '0755'
 end
 
+# Add to fstab for automatic mounting, and then kick it off
 append_if_no_line "Add to fstab" do
   path "/etc/fstab"
   line "sshfs#sanjid@atlantis.whatbox.ca:/home/sanjid/files /Atlantis fuse IdentityFile=/home/atlantis/.ssh/id_rsa,allow_other,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3, 0 0"
 end
-
 bash 'Mount atlantis' do
   user 'root'
   code 'mount  /Atlantis/'
