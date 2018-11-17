79e9c11757c05ffd57a1c6958c9095969bce1f9b
Lucas Kjaero <lucas@lucaskjaero.com>
2018-11-17 15:21:51 -0800

    Fix pihole installation on debian

--- cookbooks/docker_base/recipes/default.rb
+++ cookbooks/docker_base/recipes/default.rb
@@ -4,6 +4,12 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+# Needed to connect to docker repository since it is TLS
+package 'apt-transport-https' do
+  action :install
+  ignore_failure true
+end
+
 docker_installation_package 'default'
 
 docker_service_manager 'default' do
