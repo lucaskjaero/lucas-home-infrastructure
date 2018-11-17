2cdefc6098316362777c11c1e3de86f22f6feb83
Lucas Kjaero <lucas@lucaskjaero.com>
2018-11-17 09:02:03 -0800

    Setup for pihole

--- policies/pihole.rb
+++ policies/pihole.rb
@@ -6,3 +6,6 @@
 name 'pihole'
 default_source :supermarket
 run_list 'linux_base::default'
+
+cookbook 'linux_base', path: '../../cookbooks/linux_base'
+cookbook 'pihole', path: '../../cookbooks/pihole'
