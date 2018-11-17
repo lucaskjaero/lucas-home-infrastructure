f8dc15aaea05424bdd80f543c794c9918b2ac85f
Lucas Kjaero <lucas@lucaskjaero.com>
2018-11-17 09:04:16 -0800

    Missing a dependency

--- policies/pihole.rb
+++ policies/pihole.rb
@@ -5,7 +5,8 @@
 # A name that describes what the system you're building with Chef does.
 name 'pihole'
 default_source :supermarket
-run_list 'linux_base::default'
+run_list 'linux_base::default', 'pihole'
 
 cookbook 'linux_base', path: '../cookbooks/linux_base'
+cookbook 'docker_base', path: '../cookbooks/linux_base'
 cookbook 'pihole', path: '../cookbooks/pihole'
