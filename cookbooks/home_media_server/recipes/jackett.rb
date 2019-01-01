8bcc0f62c4336e9cfbd63d6e4d0e3a661b64a726
Lucas Kjaero <lucas@lucaskjaero.com>
2018-12-31 16:01:41 -0800

    Keep jackett from orphaning volumes

--- cookbooks/home_media_server/recipes/jackett.rb
+++ cookbooks/home_media_server/recipes/jackett.rb
@@ -5,6 +5,7 @@
 # Copyright:: 2018, Lucas Kjaero, All Rights Reserved.
 
 jackett_config_dir = "#{node["home_media_server"]["config_dir"]}/jackett"
+jackett_download_dir = node["home_media_server"]["jackett"]["download_dir"]
 
 user "jackett" do
   shell "/sbin/nologin"
@@ -22,6 +23,15 @@ directory jackett_config_dir do
   not_if { Dir.exist? jackett_config_dir }
 end
 
+# Even if we don't need use this, we need to have it to avoid orphaned volumes
+directory jackett_download_dir do
+  owner "jackett"
+  group "media_server"
+  mode "0775"
+  action :create
+  not_if { Dir.exist? jackett_download_dir }
+end
+
 docker_image "Jackett image" do
   repo "linuxserver/jackett"
   action :pull
@@ -31,7 +41,7 @@ docker_container "Jackett container" do
   container_name "jackett"
   repo "linuxserver/jackett"
   port "9117:9117"
-  volumes ["#{jackett_config_dir}:/config"]
+  volumes ["#{jackett_config_dir}:/config", "#{jackett_download_dir}:/downloads"]
   env [node["home_media_server"]["timezone"], "PUID=9117", "PGID=8888"]
   restart_policy "unless-stopped"
   action :run
