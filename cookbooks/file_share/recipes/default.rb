e5fb6f9ba415fec7ec501a58bf760b9dabba632b
Lucas Kjaero <lucas@lucaskjaero.com>
2019-01-01 12:52:36 -0800

    Cookbook for sharing files over samba

---
+++ cookbooks/file_share/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook:: file_sync
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+include_recipe "samba::server"
+
+files = node["file_share"]["share_directories"]
+
+files.each do |file|
+  samba_share file do
+    guest_ok "no"
+    write_list ["lucas"]
+    read_only "yes"
+    create_directory false
+    valid_users "lucas syncthing"
+    path file
+  end
+end
