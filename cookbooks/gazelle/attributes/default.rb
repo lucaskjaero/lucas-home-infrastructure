94c08a06cc6eea87989ea704bdb2efb31f92b74b
lkjaero <Lucas_Kjaero@intuit.com>
2019-01-04 09:46:33 -0800

    Gazelle

---
+++ cookbooks/gazelle/attributes/default.rb
@@ -0,0 +1,2 @@
+default["file_share"]["share_directories"] = ["/config"]
+default["sync"]["sync_directories"] = ["/home/lucas/Desktop:/mnt/desktop", "/files/Dropbox/Documents:/mnt/documents", "/files/Dropbox/Downloads:/mnt/downloads", "/files/Dropbox/Photos:/mnt/photos"]
---
+++ cookbooks/gazelle/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: lacabana
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
