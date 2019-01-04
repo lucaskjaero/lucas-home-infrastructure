7ca46110f5ef423b348c3128b31c30965f950ea0
lkjaero <Lucas_Kjaero@intuit.com>
2019-01-04 10:35:59 -0800

    Lacabana

--- cookbooks/lacabana/attributes/default.rb
+++ cookbooks/lacabana/attributes/default.rb
@@ -1,1 +1,3 @@
 default["file_share"]["share_directories"] = ["/config"]
+default["sync"]["sync_directories"] = ["/home/lucas/Desktop:/mnt/desktop", "/files/Documents:/mnt/documents", "/files/Downloads:/mnt/downloads", "/files/Photos:/mnt/photos"]
+
--- cookbooks/lacabana/recipes/default.rb
+++ cookbooks/lacabana/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+directory "/files" do
+  owner "root"
+  mode "0777"
+  action :create
+  not_if { Dir.exist? "/files" }
+end
+
