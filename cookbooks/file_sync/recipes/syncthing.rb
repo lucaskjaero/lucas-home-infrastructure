62ed6460f864bf7b53fb5bc6bbdbdcd985a010a2
Lucas Kjaero <lucas@lucaskjaero.com>
2019-01-04 19:04:23 -0800

    Some fixes

--- cookbooks/file_sync/recipes/syncthing.rb
+++ cookbooks/file_sync/recipes/syncthing.rb
@@ -8,6 +8,7 @@ include_recipe "docker_base"
 
 user "syncthing" do
   shell "/sbin/nologin"
+  uid 8384
   comment "Service user for syncthing"
   system true
   manage_home false
@@ -26,6 +27,13 @@ directory "/config/syncthing" do
   action :create
   not_if { Dir.exist? "/config/syncthing" }
 end
+directory "/config/syncthing/#{node.name}" do
+  owner "syncthing"
+  group "syncthing"
+  mode "0777"
+  action :create
+  not_if { Dir.exist? "/config/syncthing/#{node.name}" }
+end
 
 docker_image "Syncthing image" do
   repo "linuxserver/syncthing"
@@ -34,14 +42,15 @@ end
 
 # Mount some default folders and then allow the user to add more
 # TODO need unique mount so we don't clobber each node's data
-default_mounts = ["/config/syncthing:/config", "/config:/mnt/config/#{node.name}"]
+default_mounts = ["/config/syncthing/#{node.name}:/config", "/config:/mnt/config/"]
 user_passed_folders = node["sync"]["sync_directories"]
 syncthing_volumes = (default_mounts + user_passed_folders).uniq
 
 docker_container "Syncthing runtime" do
   container_name "syncthing"
   repo "linuxserver/syncthing"
-  port ["8384:8384", "22000:22000", "21027:21027/udp"]
+  env ['TZ="America/Los_Angeles"', "PUID=8384"]
+  network_mode "host"
   volumes syncthing_volumes
   action :run
 end
