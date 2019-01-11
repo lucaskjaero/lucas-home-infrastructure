899935827ba7f9f3ab95732fbf743df1a5817e61
Lucas Kjaero <lucas@lucaskjaero.com>
2019-01-11 07:52:43 -0800

    More syncthing directories

--- cookbooks/gazelle/attributes/default.rb
+++ cookbooks/gazelle/attributes/default.rb
@@ -1,3 +1,3 @@
 default["config_dir"] = "/home/lucas/config"
 default["file_share"]["share_directories"] = ["/config"]
-default["sync"]["sync_directories"] = ["/home/lucas/Desktop:/mnt/desktop", "/home/lucas/files/Dropbox/Documents:/mnt/documents", "/home/lucas/files/Dropbox/Downloads:/mnt/downloads", "/home/lucas/files/Dropbox/Photos:/mnt/photos"]
+default["sync"]["sync_directories"] = ["/home/lucas/files/Dropbox/Camera\ Uploads:/mnt/camera_uploads", "/home/lucas/Desktop:/mnt/desktop", "/home/lucas/files/Dropbox/Documents:/mnt/documents", "/home/lucas/files/Dropbox/Downloads:/mnt/downloads", "/home/lucas/files/Dropbox/Ebooks:/mnt/ebooks", "/home/lucas/files/Dropbox/Photos:/mnt/photos", "/home/lucas/files/Dropbox/Video:/mnt/video"]
