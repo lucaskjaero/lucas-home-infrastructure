c34643a75f404c81e2ae60b2f3b9779cf4165cf4
Lucas Kjaero <lucas@lucaskjaero.com>
2019-01-13 21:57:15 -0800

    Base lychee image

--- cookbooks/thelio/attributes/default.rb
+++ cookbooks/thelio/attributes/default.rb
@@ -1,3 +1,4 @@
 default["config_dir"] = "/config"
 default["file_share"]["share_directories"] = ["/config", "/video"]
+default["photo_server"]["photo_library_location"]
 default["sync"]["sync_directories"] = ["/video/media1/camera:/mnt/camera", "/video/media1/camera_uploads:/mnt/camera_uploads", "/home/lucas/Desktop:/mnt/desktop", "/video/media1/photos:/mnt/photos", "/video/media1/Music:/mnt/music"]
