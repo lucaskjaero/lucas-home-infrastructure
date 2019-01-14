3f9b0e593626836cd53d0f553a782f2f33fff558
Lucas Kjaero <lucas@lucaskjaero.com>
2019-01-13 21:47:19 -0800

    Some documentation

--- cookbooks/thelio/recipes/default.rb
+++ cookbooks/thelio/recipes/default.rb
@@ -3,3 +3,59 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+media1 = "/video/media1"
+media2 = "/video/media2"
+
+# Needed because the group might not have been created yet
+media_server_group = 8888
+
+# This drive is mostly meant for TV
+directory media1 do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  not_if { Dir.exist? media1 }
+end
+
+directory media2 do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  not_if { Dir.exist? media2 }
+end
+
+# TV directories section
+directory "#{media1}/TV" do
+  owner "root"
+  group media_server_group
+  mode "0775"
+  action :create
+  not_if { Dir.exist? "#{media1}/TV" }
+end
+
+directory "#{media1}/TV-Chinese" do
+  owner "root"
+  group media_server_group
+  mode "0775"
+  action :create
+  not_if { Dir.exist? "#{media1}/TV-Chinese" }
+end
+
+directory "#{media1}/TV-Danish" do
+  owner "root"
+  group media_server_group
+  mode "0775"
+  action :create
+  not_if { Dir.exist? "#{media1}/TV-Danish" }
+end
+
+directory "#{media1}/TV-Spanish" do
+  owner "root"
+  group media_server_group
+  mode "0775"
+  action :create
+  not_if { Dir.exist? "#{media1}/TV-Spanish" }
+end
