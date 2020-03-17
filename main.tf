5ef4385b6b118132987e7c42b555ef0cb9147258
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-16 23:33:15 -0700

    Proxy instance

--- main.tf
+++ main.tf
@@ -14,3 +14,12 @@ variable "digitalocean_token" {}
 provider "digitalocean" {
   token = var.digitalocean_token
 }
+
+resource "digitalocean_droplet" "lucas_proxy" {
+  image      = "ubuntu-18-04-x64"
+  name       = "lucas-proxy"
+  region     = "sfo2"
+  size       = "s-1vcpu-1gb"
+  ssh_keys   = [26855236]
+  monitoring = true
+}
