f53b228996aafd81ac8b0d383d69e9db0670bd69
Lucas Kjaero <lucas@lucaskjaero.com>
2020-05-17 12:39:18 -0700

    Missed a spot

--- cookbooks/lacabana/attributes/default.rb
+++ cookbooks/lacabana/attributes/default.rb
@@ -1,3 +1,3 @@
 default["config_dir"] = "/config"
 default["file_share"]["share_directories"] = ["/config"]
-default["sync"]["sync_directories"] = ["/home/lucas/Desktop:/mnt/desktop", "/files/camera:/mnt/camera", "/files/camera_uploads:/mnt/camera_uploads", "/files/documents:/mnt/documents", "/files/downloads:/mnt/downloads", "/files/music:/mnt/music", "/files/photos:/mnt/photos", "/files/video:/mnt/video"]
+default["sync"]["sync_directories"] = ["/home/lucas/Desktop:/mnt/desktop", "/files/camera:/mnt/camera", "/files/camera_uploads:/mnt/camera_uploads", "/files/documents:/mnt/documents", "/files/downloads:/mnt/downloads", "/files/ebooks:/mnt/ebooks", "/files/music:/mnt/music", "/files/photos:/mnt/photos", "/files/video:/mnt/video"]
