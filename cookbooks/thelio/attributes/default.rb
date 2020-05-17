97ea60224dc00906e7c1176cefb2c5d952bf2072
Lucas Kjaero <lucas@lucaskjaero.com>
2020-05-17 12:34:59 -0700

    Add ebooks

--- cookbooks/thelio/attributes/default.rb
+++ cookbooks/thelio/attributes/default.rb
@@ -1,4 +1,4 @@
 default["config_dir"] = "/config"
 default["file_share"]["share_directories"] = ["/config", "/video"]
 default["photo_server"]["photo_library_location"] = "/video/media1/photos"
-default["sync"]["sync_directories"] = ["/video/media1/camera:/mnt/camera", "/video/media1/camera_uploads:/mnt/camera_uploads", "/home/lucas/Desktop:/mnt/desktop", "/video/media1/photos:/mnt/photos", "/video/media1/Music:/mnt/music"]
+default["sync"]["sync_directories"] = ["/video/media1/camera:/mnt/camera", "/video/media1/camera_uploads:/mnt/camera_uploads", "/home/lucas/Desktop:/mnt/desktop", "/video/media1/ebooks:/mnt/ebooks", "/video/media1/photos:/mnt/photos", "/video/media1/Music:/mnt/music"]
