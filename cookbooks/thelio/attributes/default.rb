308dc76159f51fa8ce9c5687902055903507a8f4
Lucas Kjaero <lucas@lucaskjaero.com>
2020-09-23 23:52:44 -0700

    Not yet

--- cookbooks/thelio/attributes/default.rb
+++ cookbooks/thelio/attributes/default.rb
@@ -1,3 +1,3 @@
 default["config_dir"] = "/config"
 default["file_share"]["share_directories"] = ["/config", "/video"]
-default["sync"]["sync_directories"] = ["/video/media1/camera:/mnt/camera", "/video/media1/camera_uploads:/mnt/camera_uploads", "/home/lucas/Desktop:/mnt/desktop", "/video/media1/ebooks:/mnt/ebooks", "/video/media1/photos:/mnt/photos", "/video/media2/Music:/mnt/music"]
+default["sync"]["sync_directories"] = ["/video/media1/camera:/mnt/camera", "/video/media1/camera_uploads:/mnt/camera_uploads", "/home/lucas/Desktop:/mnt/desktop", "/video/media1/ebooks:/mnt/ebooks", "/video/media1/photos:/mnt/photos", "/video/media1/Music:/mnt/music"]
